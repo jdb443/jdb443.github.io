6fd8be50b789854b1c6966636f3d2b9f31e5c496
Jonathan Behar <jdb443@stern.nyu.edu>
2019-11-06 16:32:38 -0500

    Project 3

--- assets/js/jquery.countTo.js
+++ assets/js/jquery.countTo.js
@@ -1,16 +1,21 @@
-(function ($) {
-	$.fn.countTo = function (options) {
+(function($) {
+	$.fn.countTo = function(options) {
 		options = options || {};
 
-		return $(this).each(function () {
+		return $(this).each(function() {
 			// set options for current element
-			var settings = $.extend({}, $.fn.countTo.defaults, {
-				from:            $(this).data('from'),
-				to:              $(this).data('to'),
-				speed:           $(this).data('speed'),
-				refreshInterval: $(this).data('refresh-interval'),
-				decimals:        $(this).data('decimals')
-			}, options);
+			var settings = $.extend(
+				{},
+				$.fn.countTo.defaults,
+				{
+					from: $(this).data("from"),
+					to: $(this).data("to"),
+					speed: $(this).data("speed"),
+					refreshInterval: $(this).data("refresh-interval"),
+					decimals: $(this).data("decimals")
+				},
+				options
+			);
 
 			// how many times to update the value, and how much to increment the value on each update
 			var loops = Math.ceil(settings.speed / settings.refreshInterval),
@@ -21,9 +26,9 @@
 				$self = $(this),
 				loopCount = 0,
 				value = settings.from,
-				data = $self.data('countTo') || {};
+				data = $self.data("countTo") || {};
 
-			$self.data('countTo', data);
+			$self.data("countTo", data);
 
 			// if an existing interval can be found, clear it first
 			if (data.interval) {
@@ -40,41 +45,45 @@
 
 				render(value);
 
-				if (typeof(settings.onUpdate) == 'function') {
+				if (typeof settings.onUpdate == "function") {
 					settings.onUpdate.call(self, value);
 				}
 
 				if (loopCount >= loops) {
 					// remove the interval
-					$self.removeData('countTo');
+					$self.removeData("countTo");
 					clearInterval(data.interval);
 					value = settings.to;
 
-					if (typeof(settings.onComplete) == 'function') {
+					if (typeof settings.onComplete == "function") {
 						settings.onComplete.call(self, value);
 					}
 				}
 			}
 
 			function render(value) {
-				var formattedValue = settings.formatter.call(self, value, settings);
+				var formattedValue = settings.formatter.call(
+					self,
+					value,
+					settings
+				);
 				$self.text(formattedValue);
 			}
 		});
 	};
 
 	$.fn.countTo.defaults = {
-		from: 0,               // the number the element should start at
-		to: 0,                 // the number the element should end at
-		speed: 1000,           // how long it should take to count between the target numbers
-		refreshInterval: 100,  // how often the element should be updated
-		decimals: 0,           // the number of decimal places to show
-		formatter: formatter,  // handler for formatting the value before rendering
-		onUpdate: null,        // callback method for every time the element is updated
-		onComplete: null       // callback method for when the element finishes updating
+		from: 0, // the number the element should start at
+		to: 0, // the number the element should end at
+		speed: 1000, // how long it should take to count between the target numbers
+		refreshInterval: 100, // how often the element should be updated
+		decimals: 0, // the number of decimal places to show
+		formatter: formatter, // handler for formatting the value before rendering
+		onUpdate: null, // callback method for every time the element is updated
+		onComplete: null // callback method for when the element finishes updating
 	};
 
 	function formatter(value, settings) {
 		return value.toFixed(settings.decimals);
 	}
-}(jQuery));
+})(jQuery);
--- assets/js/moment.js
+++ assets/js/moment.js
@@ -2,14 +2,14 @@
 // {
 //   // instantiate a moment object
 //   var NowMoment = moment();
-  
+
 //   // instantiate a JavaScript Date object
 //   var NowDate = new Date();
-  
+
 //   // display value of moment object in #displayMoment div
 //   var eDisplayMoment = document.getElementById('displayMoment');
 //   eDisplayMoment.innerHTML = NowMoment.format('YYYY');
-  
+
 //   // display value of Date object in #displayJsDate div
 //   var eDisplayDate = document.getElementById('displayJsDate');
 //   eDisplayDate.innerHTML = NowDate;
